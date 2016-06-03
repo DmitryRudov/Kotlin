9b34b3962ca4214c793ebde9ba1a389e3fc7d912
Rudov Dmitry <rudovdmitry@gmail.com>
2016-06-03 06:16:39 +0300

    The task9 complite. Extention functions

--- src/i_introduction/_9_Extension_Functions/ExtensionFunctions.kt
+++ src/i_introduction/_9_Extension_Functions/ExtensionFunctions.kt
@@ -3,18 +3,6 @@ package i_introduction._9_Extension_Functions
 import util.TODO
 import util.doc9
 
-fun String.lastChar() = this.get(this.length - 1)
-
-// 'this' can be omitted
-fun String.lastChar1() = get(length - 1)
-
-fun use() {
-    // try Ctrl+Space "default completion" after the dot: lastChar() is visible
-    "abc".lastChar()
-}
-
-// 'lastChar' is compiled to a static function in the class ExtensionFunctionsKt (see JavaCode9.useExtension)
-
 fun todoTask9(): Nothing = TODO(
     """
         Task 9.
@@ -27,7 +15,8 @@ fun todoTask9(): Nothing = TODO(
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
-fun Int.r(): RationalNumber = todoTask9()
-fun Pair<Int, Int>.r(): RationalNumber = todoTask9()
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.first, this.second)
+
 
 
